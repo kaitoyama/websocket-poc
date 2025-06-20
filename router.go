c4e0816fdbbc8ee6150fd8c1dc5b6c8dc51cc0eb
kaitoyama <uitakato@gmail.com>
2025-06-20 10:23:14 +0900

    feat: embed OpenAPI spec and static files in SetupRouter function

--- router.go
+++ router.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"embed"
+	"io/fs"
 	"log"
-	"os"
+	"net/http"
 
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/kaitoyama/kaitoyama-server-template/internal/infrastructure/db"
@@ -12,14 +14,20 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+//go:embed openapi/swagger.yml
+var swaggerFS embed.FS
+
+//go:embed static/*
+var staticFS embed.FS
+
 func SetupRouter(database *sql.DB) *echo.Echo {
 	// Initialize Echo
 	e := echo.New()
 
-	// Load OpenAPI spec from file
-	swaggerBytes, err := os.ReadFile("./openapi/swagger.yml")
+	// Load OpenAPI spec from embedded file
+	swaggerBytes, err := swaggerFS.ReadFile("openapi/swagger.yml")
 	if err != nil {
-		log.Fatalf("Failed to read swagger file: %v", err)
+		log.Fatalf("Failed to read embedded swagger file: %v", err)
 	}
 	loader := openapi3.NewLoader()
 	swagger, err := loader.LoadFromData(swaggerBytes)
@@ -42,8 +50,12 @@ func SetupRouter(database *sql.DB) *echo.Echo {
 	e.GET("/ws", healthHandler.HandleWebSocket)
 	e.GET("/ws/rooms", healthHandler.GetWebSocketRooms)
 
-	// Static files
-	e.Static("/static", "./static")
+	// Static files from embedded filesystem
+	staticSubFS, err := fs.Sub(staticFS, "static")
+	if err != nil {
+		log.Fatalf("Failed to create sub filesystem for static files: %v", err)
+	}
+	e.GET("/static/*", echo.WrapHandler(http.FileServer(http.FS(staticSubFS))))
 
 	return e
 }
